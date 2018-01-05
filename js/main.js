90e2a72f38a9c4b5272f60843e2282997088eec2
Sudha <msrghvn@gmail.com>
2018-01-04 23:30:13 -0500

    Removed gifs

--- js/main.js
+++ js/main.js
@@ -77,10 +77,10 @@ BRUSHED.slider = function(){
 		thumb_links				:	0,			// Individual thumb links for each slide
 		thumbnail_navigation    :   1,			// Thumbnail navigation
 		slides 					:  	[			// Slideshow Images
-											{image : 'img/slider-images/Round1-optimized-gif.gif', title : '<div class="slide-content">Portfolio</div>', thumb : 'img/slider-images/Round1-optimized-gif.gif', url : ''},
-											{image : 'img/slider-images/Round2-gif.gif', title : '<div class="slide-content">Portfolio</div>', thumb : 'img/slider-images/Round2-gif.gif', url : ''},
+											//{image : 'img/slider-images/Round1-optimized-gif.gif', title : '<div class="slide-content">Portfolio</div>', thumb : 'img/slider-images/Round1-optimized-gif.gif', url : ''},
+											//{image : 'img/slider-images/Round2-gif.gif', title : '<div class="slide-content">Portfolio</div>', thumb : 'img/slider-images/Round2-gif.gif', url : ''},
 											//{image : 'img/slider-images/Round4-gif.gif', title : '<div class="slide-content">Portfolio</div>', thumb : 'img/slider-images/Round4-gif.gif', url : ''},											
-											{image : 'img/slider-images/Togo-gif.gif', title : '<div class="slide-content">Portfolio</div>', thumb : 'img/slider-images/Togo-gif.gif', url : ''}
+											//{image : 'img/slider-images/Togo-gif.gif', title : '<div class="slide-content">Portfolio</div>', thumb : 'img/slider-images/Togo-gif.gif', url : ''}
 									],
 									
 		// Theme Options			   
